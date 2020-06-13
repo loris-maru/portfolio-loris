08fce8ca488b2a8215b1634d27b57a6c88e68027
loris-maru <admin@lo-ol.design>
2020-06-13 12:36:18 +0200

    Changin useCdn of the sanity.js file

--- src/sanity.js
+++ src/sanity.js
@@ -3,5 +3,6 @@ import sanityClient from '@sanity/client'
 export default sanityClient({
   projectId: 's3jiyh9l',
   dataset: 'production',
-  useCdn: true
+  //useCdn: true
+  useCdn: process.env.NODE_ENV === 'production'
 })
